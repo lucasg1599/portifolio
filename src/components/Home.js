2934635866bfd80f91dc3ad886d99ce669de1ced
lucasg1599 <lucasg1599@gmail.com>
2025-01-22 20:49:26 -0300

    teste

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div id="home" style={{ height: "100vh", backgroundColor: "#3498db", color: "#fff" }}>
+    <div id="home" style={{ height: "10vh", backgroundColor: "#3498db", color: "#fff" }}>
       <div
         style={{
           height: "100%",
